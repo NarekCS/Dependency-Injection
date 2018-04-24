a94fd0c8be048e5e7104a84005a0fb719e9375de
NarekCS <33220145+NarekCS@users.noreply.github.com>
2018-04-25 03:02:09 +0400

    Action Injection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,26 +5,27 @@ using System.Threading.Tasks;
 using DependencyInjection.Infrastructure;
 using DependencyInjection.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
-    {
-
+    {        
         public HomeController(IRepository repo, ProductTotalizer total)
         {
             repository = repo;
             totalizer = total;
         }
-
+         
         private ProductTotalizer totalizer;
         private IRepository repository;// { get; } = TypeBroker.Repository;
 
         
-        public ViewResult Index()
-        {
+        public ViewResult Index()//[FromServices]ProductTotalizer totalizer)
+        {            
+            //IRepository repository = HttpContext.RequestServices.GetRequiredService<IRepository>();
             ViewBag.HomeController = repository.ToString();
             ViewBag.Totalizer = totalizer.Repository.ToString();
             return View(repository.Products);
